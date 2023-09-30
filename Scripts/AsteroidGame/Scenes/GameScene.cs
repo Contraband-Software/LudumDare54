da990e62a512f2c402d904ad485fcdffffaba225
Samuel Taseff <samtaseff@gmail.com>
2023-09-30 18:32:04 +0100

    More main app dev

--- Scripts/AsteroidGame/Scenes/GameScene.cs
+++ Scripts/AsteroidGame/Scenes/GameScene.cs
@@ -9,6 +9,8 @@ using Microsoft.Xna.Framework.Graphics;
 
 public class GameScene : Scene
 {
+    private int sunLight = -1;
+
     public GameScene(Game appCtx) : base("GameScene", appCtx)
     {
 
@@ -18,10 +20,12 @@ public class GameScene : Scene
     {
         Texture2D blankTexure = this.contentManager.Load<Texture2D>("Sprites/block");
 
-        DebugPlayer playerBlock = new DebugPlayer(blankTexure, "DebugPlayerController", this.app);
-        parentObject.AddChild(playerBlock);
+        DebugPlayer player = new(blankTexure, "DebugPlayerController", this.app);
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+        parentObject.AddChild(player);
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
 
-        this.app.Services.GetService<ILeviathanEngineService>().AddLight(new Vector2(200, 200), new Vector3(10000000, 10000000, 10000000));
+        sunLight = this.app.Services.GetService<ILeviathanEngineService>().AddLight(new Vector2(200, 200), new Vector3(10000000, 10000000, 10000000));
     }
 
     public override void Update(GameTime gameTime)
@@ -29,5 +33,8 @@ public class GameScene : Scene
         base.Update(gameTime);
     }
 
-    public override void OnUnload() => throw new System.NotImplementedException();
+    public override void OnUnload()
+    {
+        this.app.Services.GetService<ILeviathanEngineService>().removeLight(this.sunLight);
+    }
 }
