153dfd60103765a300017075d2cea637b3d68da9
Samuel Taseff <samtaseff@gmail.com>
2023-10-02 21:40:07 +0100

    STart scene

--- Scripts/AsteroidGame/GameObjects/TitleScreenSystem.cs
+++ Scripts/AsteroidGame/GameObjects/TitleScreenSystem.cs
@@ -8,33 +8,88 @@ public class TitleScreenSystem : GameObject
 {
     private ILeviathanEngineService? render;
 
-    private SpriteFont font;
+    private SpriteFont titleFont;
+    private SpriteFont subTitleFont;
 
-    private LeviathanUIElement gameTitle;
+    private readonly int showTime = 0;
+    private float timeShowed = 0;
 
-    public TitleScreenSystem(SpriteFont font, Game appCtx) : base("TitleScreenObject", appCtx)
+    public TitleScreenSystem(int showTime, SpriteFont titleFont, SpriteFont subtitleFont, Game appCtx) : base("TitleScreenObject", appCtx)
     {
-        this.font = font;
+        this.titleFont = titleFont;
+        this.subTitleFont = subtitleFont;
+        this.showTime = showTime;
     }
 
+    private LeviathanUIElement gameTitle;
+    private LeviathanUIElement gameSubtitle1;
+    private LeviathanUIElement gameSubtitle2;
     public override void OnLoad(GameObject? parentObject)
     {
         render = this.app.Services.GetService<ILeviathanEngineService>();
 
-        Vector2 titlePos = this.render.getWindowSize() / 2f;
-        string titleText = "EVENT HORIZON";
-        float titleScale = 1;
+        {
+            Matrix fuckingTitlePosition = this.GetGlobalTransform();
 
-        Matrix fuckingTitlePosition = this.GetGlobalTransform();
-        fuckingTitlePosition.Translation += new Vector3(titlePos - font.MeasureString(titleText) / 2 * titleScale, 0);
-        gameTitle = new LeviathanUIElement(this.app, fuckingTitlePosition, new Vector2(titleScale), titleText, this.font, Color.White);
+            Vector2 titlePos = this.render.getWindowSize() / 2f;
+            string titleText = "EVENT HORIZON";
+            float titleScale = 1;
+            fuckingTitlePosition.Translation += new Vector3(titlePos - titleFont.MeasureString(titleText) / 2 * titleScale, 0);
+            gameTitle = new LeviathanUIElement(this.app, fuckingTitlePosition, new Vector2(titleScale), titleText, this.titleFont, Color.White);
+        }
+        {
+            Matrix fuckingTitlePosition = this.GetGlobalTransform();
 
-        this.render.addUISprite(gameTitle);
+            Vector2 subTitle1Pos = this.render.getWindowSize() / 2f + new Vector2(0, 100);
+            string subTitle1Text = "By Contraband Studio, 2023, for Ludum Dare";
+            fuckingTitlePosition.Translation += new Vector3(subTitle1Pos - subTitleFont.MeasureString(subTitle1Text) / 2, 0);
+            gameSubtitle1 = new LeviathanUIElement(this.app, fuckingTitlePosition, new Vector2(1), subTitle1Text, this.subTitleFont, Color.White);
+        }
+        {
+            Matrix fuckingTitlePosition = this.GetGlobalTransform();
+
+            Vector2 subTitle1Pos = this.render.getWindowSize() / 2f + new Vector2(0, 200);
+            string subTitle1Text = "With the Poo Machine Game Engine";
+            fuckingTitlePosition.Translation += new Vector3(subTitle1Pos - subTitleFont.MeasureString(subTitle1Text) / 2, 0);
+            gameSubtitle2 = new LeviathanUIElement(this.app, fuckingTitlePosition, new Vector2(1), subTitle1Text, this.subTitleFont, Color.White);
+        }
+    }
+
+    private bool showedTitle = false;
+    private bool showedEngine = false;
+    private bool showedStudio = false;
+    public override void Update(GameTime gameTime)
+    {
+        int increment = this.showTime / 4;
+
+        timeShowed = gameTime.TotalGameTime.Seconds;
+
+        if (this.timeShowed > increment && !showedTitle)
+        {
+            showedTitle = true;
+            this.render.addUISprite(gameTitle);
+        } else if (this.timeShowed > increment * 2 && !showedStudio)
+        {
+            showedStudio = true;
+            this.render.addUISprite(gameSubtitle1);
+
+        } else if (this.timeShowed > increment * 3 && !showedEngine)
+        {
+            showedEngine = true;
+            this.render.addUISprite(gameSubtitle2);
+        } else if (this.timeShowed > increment * 4)
+        {
+            this.app.Services.GetService<ISceneControllerService>().ChangeScene("GameScene");
+        }
+
+        base.Update(gameTime);
     }
 
     public override void OnUnload()
     {
         this.render.removeUISprite(gameTitle);
+        this.render.removeUISprite(gameSubtitle1);
+        this.render.removeUISprite(gameSubtitle2);
 
         base.OnUnload();
     }
